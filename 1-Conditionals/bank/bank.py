05990946863474d3ec43d4563f7b7eb975b6c524
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-02-12 02:51:57 +0000

    Bank.py

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,12 @@
+def main():
+    greeting = input("Hello! ").lower()
+
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+main()
