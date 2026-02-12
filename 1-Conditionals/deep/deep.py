26c10e6d424d54a3dc7ff33c15e7fe76f8de4985
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-02-12 02:47:46 +0000

    deep.py

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,10 @@
+def main():
+    life = input("What is the meaning of life?").lower()
+
+    if life == "42" or life == "forty two" or life == "forty-two":
+        print("yes")
+    else:
+        print("no")
+
+
+main()
---
+++ compare.py
@@ -0,0 +1,7 @@
+x = int(input("what is x?"))
+y = int(input("what is y?"))
+
+if x < y or x > y:
+    print("x is not y")
+else:
+    print("x is y")
