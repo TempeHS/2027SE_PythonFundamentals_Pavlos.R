491a23223bee93ed76aa269cbcd3c1e6dac3aae2
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-02-17 02:31:45 +0000

    twttr.py

--- 2-Loops/twttr/twttr.py
+++ 2-Loops/twttr/twttr.py
@@ -2,9 +2,10 @@ def main():
     text = input("What's your tweet? ")
     for i in text:
         if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
-
+            print(end="")
         else:
             print(i, end="")
+    print()
 
 
 main()
