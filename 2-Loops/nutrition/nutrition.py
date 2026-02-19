cb2de656c782d3bba8da95866f3139b1e3114090
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-02-19 03:06:47 +0000

    nutrition.py

---
+++ 2-Loops/nutrition/nutrition.py
@@ -0,0 +1,44 @@
+list = [
+    {"name": "apple", "calories": "130"},
+    {"name": "avocado", "calories": "50"},
+    {"name": "banana", "calories": "110"},
+    {"name": "cantaloupe", "calories": "50"},
+    {"name": "grapefruit", "calories": "60"},
+    {"name": "grapes", "calories": "90"},
+    {"name": "honeydew melon", "calories": "50"},
+    {"name": "kiwi fruit", "calories": "90"},
+    {"name": "lemon", "calories": "15"},
+    {"name": "lime", "calories": "20"},
+    {"name": "nectarine", "calories": "60"},
+    {"name": "orange", "calories": "80"},
+    {"name": "peach", "calories": "60"},
+    {"name": "pear", "calories": "100"},
+    {"name": "pineapple", "calories": "50"},
+    {"name": "plums", "calories": "70"},
+    {"name": "strawberries", "calories": "50"},
+    {"name": "sweet cherries", "calories": "100"},
+    {"name": "tangerine", "calories": "50"},
+    {"name": "watermelon", "calories": "80"},
+]
+
+
+def main():
+
+    fruit = input("What is your fruit? ")
+
+    if search(fruit.lower()) is not None:
+        print(search(fruit.lower()))
+    else:
+        print("Invalid Fruit!")
+
+
+def search(string):
+    for i in list:
+
+        if i["name"] == string:
+            return i["calories"]
+
+    return None
+
+
+main()
