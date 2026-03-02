046e0020801b385054980c4ea2b5b55e9899ac2b
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-03-02 22:16:03 +0000

    Figlet.py

--- 4-Libraries/figlet/figlet.py
+++ 4-Libraries/figlet/figlet.py
@@ -1,17 +1,45 @@
 import sys
-import pyfiglet import figlet
+import random
+from pyfiglet import Figlet
+
+
 figlet = Figlet()
+
 
 def main():
 
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
 
+        rand_font()
 
-
-    elif len(sys.argv) == 2:
+    elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
         font(sys.argv[2])
 
+    else:
+        print("invalid usage")
+        sys.exit()
 
 
 def font(f):
-    figlet.setFont(font=f)  
+    if f in figlet.getFonts():
+
+        figlet.setFont(font=f)
+        prompt()
+    else:
+        print("invalid")
+
+
+def rand_font():
+
+    f = random.choice(figlet.getFonts())
+    figlet.setFont(font=f)
+    prompt()
+
+
+def prompt():
+
+    text = input("What would you like to say? \n")
+    print(figlet.renderText(text))
+
+
+main()
