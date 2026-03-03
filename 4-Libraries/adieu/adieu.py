408d8aed04cc09a4a65b2f88c0049ae335ca0582
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-03-03 02:36:59 +0000

    Adieu.py

--- 4-Libraries/adieu/adieu.py
+++ 4-Libraries/adieu/adieu.py
@@ -7,10 +7,21 @@ names = []
 
 def main():
 
-
     while True:
 
         try:
-            name = input("State a name")
-            
+            name = input("State a name. ")
+            names.append(name)
+            continue
+        except (KeyboardInterrupt, EOFError):
+            break
 
+    output_print()
+
+
+def output_print():
+    joined_output = p.join(names)
+    print(f"Adieu, Adieu, to {joined_output}")
+
+
+main()
