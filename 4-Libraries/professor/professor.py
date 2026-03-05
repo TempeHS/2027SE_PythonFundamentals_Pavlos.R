8d0a7f1d3b5910e76bac88a56b0d135d74ec8d7d
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-03-05 02:54:44 +0000

    professor.py

--- 4-Libraries/professor/professor.py
+++ 4-Libraries/professor/professor.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 
 def main():
@@ -22,8 +23,34 @@ def get_level():
 
 
 def generate_integer(level):
+    question_num = 1
+    lives = 3
+    score = 0
+    while question_num <= 10:
+        correct = False
 
-
+        num_1 = random.randint(1, (1 * pow(10, level)) - 1)
+        num_2 = random.randint(1, (1 * pow(10, level)) - 1)
+        answer = num_1 + num_2
+        while correct == False:
+            if lives <= 0:
+                print(f"Your score was: {score}")
+                sys.exit()
+            print(f"What is {num_1} + {num_2}?")
+            try:
+                user_answer = int(input())
+            except ValueError:
+                continue
+            else:
+                if user_answer != answer:
+                    lives -= 1
+                    print("EEE")
+                else:
+                    correct = True
+                    question_num += 1
+                    score += 1
+    print(score)
+    sys.exit()
 
 
 if __name__ == "__main__":
